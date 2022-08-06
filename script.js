960650ee53c6760192c7eb1e1e3c11729b0a5d82
Bonjardim <wellingtonbonjardim7@gmail.com>
2022-08-06 13:27:57 -0300

    Adicionando novas músicas

--- script.js
+++ script.js
@@ -14,11 +14,36 @@ let indexMusica = 0
 
 //----------------------- BANCO DE MÚSICAS ----------------------- //
 let bancoMusicas = [
+    {titulo:'Somebody to Love', artista: 'Queen', src: 'musicas/Somebody to Love.mp3', img:'imagens/Somebody.jpg'},
+
     {titulo:'Plush', artista: 'Stone Temple Pilots', src: 'musicas/Plush (2017 Remaster).mp3', img:'imagens/plush.jpg'},
 
     {titulo:'Outshined', artista: 'Soundgarden', src: 'musicas/Soundgarden - Outshined.mp3', img:'imagens/outshined.jpg'},
 
-    {titulo:'Bulls on Parade', artista: 'Rage Against the Machine', src: 'musicas/Rage Against The Machine - Bulls On Parade (Official Music Video).mp3', img:'imagens/bulls.jpg'}
+    {titulo:'Bulls on Parade', artista: 'Rage Against the Machine', src: 'musicas/Bulls On Parade.mp3', img:'imagens/bulls.jpg'},
+
+    {titulo:'Them Bones', artista: 'Alice in Chains', src: 'musicas/Them Bones.mp3', img:'imagens/dirt.jpg'},
+
+    {titulo:'Dream On', artista: 'Aerosmith', src: 'musicas/Dream On.mp3', img:'imagens/Aerosmith.jpg'},
+
+    {titulo:'Like a Stone', artista: 'Audioslave', src: 'musicas/Like a Stone.mp3', img:'imagens/Audioslave.jpg'},
+
+    {titulo:'5 Minutes Alone', artista: 'Pantera', src: 'musicas/5 Minutes Alone.mp3', img:'imagens/5minutes.jpg'},
+
+    {titulo:'Bohemian Rhapsody', artista: 'Queen', src: 'musicas/Bohemian Rhapsody.mp3', img:'imagens/Bohemian.jpg'},
+
+    {titulo:'Dead and Bloated', artista: 'Stone Temple Pilots', src: 'musicas/Dead and Bloated.mp3', img:'imagens/plush.jpg'},
+
+    {titulo:'Rusty Cage', artista: 'Soundgarden', src: 'musicas/Rusty Cage.mp3', img:'imagens/outshined.jpg'},
+
+    {titulo:'Killing In the Name', artista: 'Rage Against the Machine', src: 'musicas/Killing In the Name.mp3', img:'imagens/Killing.jpg'},
+
+    {titulo:'Man in the Box', artista: 'Alice in Chains', src: 'musicas/Man In the Box.mp3', img:'imagens/facelift.jpg'},
+
+    {titulo:'Show me how to live', artista: 'Audioslave', src: 'musicas/show me.mp3', img:'imagens/Audioslave.jpg'},
+
+    {titulo:'Hollow', artista: 'Pantera', src: 'musicas/Hollow.mp3', img:'imagens/Hollow.jpg'}
+
 ]
 
 //----------------------- FUNÇÕES ----------------------- //
@@ -76,7 +101,7 @@ musica.addEventListener('loadeddata', corrigeNAN)
 botaoVoltar.addEventListener('click', () => {
     indexMusica--
     if(indexMusica < 0) {
-        indexMusica = 2
+        indexMusica = 14
     }
     botaoPlay.classList.remove('invisible')
     botaoPausar.classList.add('invisible')
@@ -84,7 +109,7 @@ botaoVoltar.addEventListener('click', () => {
 })
 botaoAvancar.addEventListener('click', () => {
     indexMusica++
-    if(indexMusica > 2) {
+    if(indexMusica > 14) {
         indexMusica = 0
     }
     botaoPlay.classList.remove('invisible')
