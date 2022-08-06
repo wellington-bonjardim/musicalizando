4cdef9753794883f9d8b34d2424a058724baa515
Bonjardim <wellingtonbonjardim7@gmail.com>
2022-08-06 11:47:35 -0300

    Lógica para avançar e voltar músicas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//----------------------- VARIÁVEIS ----------------------- //
 let musica = document.querySelector('audio')
 let botaoVoltar = document.querySelector('.botao-voltar')
 let botaoPlay = document.querySelector('.botao-play')
@@ -9,6 +10,27 @@ let tempoTotal = document.querySelector('.fim')
 let imagem = document.querySelector('img')
 let nomeMusica = document.querySelector('.descricao h2')
 let nomeArtista = document.querySelector('.descricao i')
+let indexMusica = 0
+
+//----------------------- BANCO DE MÚSICAS ----------------------- //
+let bancoMusicas = [
+    {titulo:'Plush', artista: 'Stone Temple Pilots', src: 'musicas/Plush (2017 Remaster).mp3', img:'imagens/plush.jpg'},
+
+    {titulo:'Outshined', artista: 'Soundgarden', src: 'musicas/Soundgarden - Outshined.mp3', img:'imagens/outshined.jpg'},
+
+    {titulo:'Bulls on Parade', artista: 'Rage Against the Machine', src: 'musicas/Rage Against The Machine - Bulls On Parade (Official Music Video).mp3', img:'imagens/bulls.jpg'}
+]
+
+//----------------------- FUNÇÕES ----------------------- //
+function renderizarMusica(index) {
+    musica.setAttribute('src', bancoMusicas[index].src)
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = bancoMusicas[index].titulo
+        nomeArtista.textContent =  bancoMusicas[index].artista
+        imagem.src =  bancoMusicas[index].img
+        tempoTotal.textContent = atualizaTempo(Math.floor(musica.duration))   
+    })
+}
 
 function tocarMusica() {
     musica.play()
@@ -22,14 +44,6 @@ function pausarMusica() {
     botaoPlay.classList.toggle('invisible')
 }
 
-function voltarMusica() {
-    
-}
-
-function avancarMusica() {
-
-}
-
 function tempoMusica() {
     barraTempo.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
 
@@ -40,24 +54,40 @@ function atualizaTempo(segundos) {
     let campoMinutos = Math.floor(segundos / 60)
     let campoSegundos = segundos % 60
     
-    if(campoSegundos < 10 && campoMinutos < 10) {
+    if(campoSegundos < 10) {
         campoSegundos = '0' + campoSegundos
+    }
+    if(campoMinutos < 10) {
         campoMinutos = '0' + campoMinutos
     }
-    
+
     return campoMinutos+':'+campoSegundos
 }
 
-function duracao() {
+function corrigeNAN() {
     tempoTotal.textContent = atualizaTempo(Math.floor(musica.duration))
-    /*Essa função serviu para corrigir o bug do tempo total que estava aparecendo apenas NaN */
 }
 
+//----------------------- EVENTOS ----------------------- //
 botaoPlay.addEventListener('click', tocarMusica)
 botaoPausar.addEventListener('click', pausarMusica)
-botaoVoltar.addEventListener('click', voltarMusica)
-botaoAvancar.addEventListener('click', avancarMusica)
 musica.addEventListener('timeupdate', tempoMusica)
-musica.addEventListener('loadeddata', duracao)
-
-//Desenvolver uma lógica para pular e voltar as músicas!
+musica.addEventListener('loadeddata', corrigeNAN)
+botaoVoltar.addEventListener('click', () => {
+    indexMusica--
+    if(indexMusica < 0) {
+        indexMusica = 2
+    }
+    botaoPlay.classList.remove('invisible')
+    botaoPausar.classList.add('invisible')
+    renderizarMusica(indexMusica)
+})
+botaoAvancar.addEventListener('click', () => {
+    indexMusica++
+    if(indexMusica > 2) {
+        indexMusica = 0
+    }
+    botaoPlay.classList.remove('invisible')
+    botaoPausar.classList.add('invisible')
+    renderizarMusica(indexMusica)
+})
